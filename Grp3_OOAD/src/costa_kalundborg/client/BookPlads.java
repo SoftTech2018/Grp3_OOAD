0c9c9646e5094ddc0195896736bde0201b9898ce
JacobWorckJepsen <JacobWorckJepsen@10.16.137.34>
2015-10-29 16:42:14 +0100

    Tilføjelse: Dropdown menu

--- Grp3_OOAD/src/costa_kalundborg/client/BookPlads.java
+++ Grp3_OOAD/src/costa_kalundborg/client/BookPlads.java
@@ -50,29 +50,31 @@ public class BookPlads extends Composite {
 		date1 = new TextBox();
 		ft.setWidget(0, 1, date1);
 		
-		startDate = date1.getText();
 		
 		ft.setText(1, 0, "Slut dato");
 		date2 = new TextBox();
 		ft.setWidget(1, 1, date2);
 		
-		endDate = date2.getText();
 		
 		ft.setText(2, 0, "Antal voksne");
 		adult = new TextBox();
 		ft.setWidget(2, 1, adult);
-		adults = Integer.parseInt(adult.getText());
+		
 		
 		ft.setText(3, 0, "Antal børn");
 		child = new TextBox();
 		ft.setWidget(3, 1, child);
-		children = Integer.parseInt(child.getText());
+		
 		
 		ok = new Button("Søg");
 		ok.addClickHandler(new ClickHandler(){
 			
 			@Override
 			public void onClick(ClickEvent event) {
+				startDate = date1.getText();
+				endDate = date2.getText();
+				adults = Integer.parseInt(adult.getText());
+				children = Integer.parseInt(child.getText());
 				try {
 					Status ledig = Status.CANCEL;
 					service.checkBooking(new BookingDTO(startDate, endDate, ledig, 0.0, 0, 0, 0, adults, children, null, null), new AsyncCallback<List<PladsDTO>>(){
@@ -85,8 +87,11 @@ public class BookPlads extends Composite {
 
 						@Override
 						public void onSuccess(List<PladsDTO> result) {
-							// TODO Auto-generated method stub
-							
+							type = new ListBox();
+							for (PladsDTO i : result){
+								type.addItem(getDescription(i.getPlads_id()));
+							}
+							ft.setWidget(5, 0, type);
 						}
 						
 					});
@@ -148,28 +153,32 @@ public class BookPlads extends Composite {
 		vPane.add(ft);
 	}
 	
-//	private PladsDTO getResidenceType(int n) throws Exception{
-//		
-//		switch(n) {
-//		case 0:
-//			return new PladsDTO(0, "LILLE_TELT", 82.0, 87.0);
-//		case 1:
-//			return new PladsDTO(0, "STORT_TELT", 82.0, 87.0);
-//		case 2:
-//			return new PladsDTO(0, "LILLE_TELT", 82.0, 87.0);
-//		case 3:
-//			break;
-//		case 4:
-//			break;
-//		case 5:
-//			break;
-//		case 6:
-//			break;
-//			
-//			
-//		}
-//		
-//		return null;
-//	}
+	
+	private String getDescription(int n){
+		
+		switch(n) {
+		case 1:
+			return new String("1, 'Lille camping', 400.00, 350.00");
+		case 2:
+			return new String("2, 'Lille camping', 400.00, 350.00");
+		case 3:
+			return new String("3, 'Lille camping', 400.00, 350.00");
+		case 4:
+			return new String("4, 'Stor camping', 740.00, 350.00");
+		case 5:
+			return new String("5, 'Stor camping', 740.00, 350.00");
+		case 6:
+			return new String("6, 'Stor hytte', 980.00, 350.00");
+		case 7:
+			return new String("7, 'Stor hytte', 980.00, 350.00");
+		case 8:
+			return new String("8, 'Lille hytte', 680.00, 350.00");
+		case 9:
+			return new String("9, 'Lille hytte', 680.00, 350.00");		
+		}
+		
+		return null;
+	}
 
 }
+
